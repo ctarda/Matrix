c68e636fd04f719d33c4e30fc66d9bb56e4befb6
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-05-23 12:16:14 +0800

    Initial Commit

---
+++ Matrix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Matrix
+//
+//  Created by Cesar Tardaguila on 23/5/2020.
+//  Copyright © 2020 ctarda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatrixTests/MatrixTests.swift
@@ -0,0 +1,34 @@
+//
+//  MatrixTests.swift
+//  MatrixTests
+//
+//  Created by Cesar Tardaguila on 23/5/2020.
+//  Copyright © 2020 ctarda. All rights reserved.
+//
+
+import XCTest
+@testable import Matrix
+
+class MatrixTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
